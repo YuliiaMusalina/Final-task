73eb66e33b37386c5e1912230222bd6d917d06e7
Ulia Kamalova <mysula@mail.ru>
2022-09-21 00:00:21 +0500

    Задача

---
+++ Task Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] symbols = { "1234", "1567", "-2", "computer science" };
+string[] newSymbols = new string[symbols.Length];
+int count = 0;
+for (int i = 0; i < symbols.Length; i++)
+{
+    if (symbols[i].Length <= 3)
+    {
+        newSymbols[count] = symbols[i];
+        count++;
+
+    }
+
